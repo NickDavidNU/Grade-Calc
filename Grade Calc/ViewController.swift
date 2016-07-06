041438fa08bc550032da18ef3d879648a31e282a
Nick Andrew David <ndavid126@gmail.com>
2016-07-06 10:13:12 -0500

    first version

--- Grade Calc/ViewController.swift
+++ Grade Calc/ViewController.swift
@@ -9,11 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+ 
+    @IBOutlet var val1: UITextField!
+    @IBOutlet var val2: UITextField!
+    @IBAction func dval1(sender: UITextField) {
+        let duck=Float(val1.text!);
+        val3.value=duck!;
+        n60.text="In Order to get a 60% you need..." 
+    }
+    @IBOutlet var val3: UISlider!
+    @IBOutlet var val4: UISlider!
+    @IBAction func dval3(sender: UISlider) {
+        
+        val1.text=val3.value.description;
+    }
+    @IBAction func dval4(sender: AnyObject) {
+        val2.text=val4.value.description;
 
+    }
+  
+    @IBAction func dval2(sender: UITextField) {
+        let duck1=Float(val2.text!);
+        val4.value=duck1!;
+    }
+    @IBOutlet var n60: UILabel!
+    @IBOutlet var n70: UILabel!
+    @IBOutlet var n80: UILabel!
+    @IBOutlet var n83: UILabel!
+    @IBOutlet var n87: UILabel!
+    @IBOutlet var n90: UILabel!
+    @IBOutlet var n93: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBOutlet var Test: UITableView!
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
